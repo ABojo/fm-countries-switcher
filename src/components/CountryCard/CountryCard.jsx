f42a464b35b6ad1e37c426ee9aff6cf687ed42b4
Alex Bojorquez <Alex.bojorquez2598@gmail.com>
2023-03-13 16:47:33 -0400

    built the country card component

---
+++ src/components/CountryCard/CountryCard.jsx
@@ -0,0 +1,36 @@
+import {
+  Container,
+  Flag,
+  Body,
+  Title,
+  Data,
+  DataName,
+} from "./CountryCard.styles";
+
+function CountryCard({ country }) {
+  const { population, region } = country;
+  const name = country.name.common;
+  const capital = country.capital;
+  const flagUrl = country.flags.png;
+  const countryUrl = `/country/${name.toLowerCase()}`;
+
+  return (
+    <Container>
+      <Flag src={flagUrl}></Flag>
+      <Body>
+        <Title to={countryUrl}>{name}</Title>
+        <Data>
+          <DataName>Population:</DataName> {population}
+        </Data>
+        <Data>
+          <DataName>Region:</DataName> {region}
+        </Data>
+        <Data>
+          <DataName>Capital:</DataName> {capital}
+        </Data>
+      </Body>
+    </Container>
+  );
+}
+
+export default CountryCard;
---
+++ src/components/CountryCard/CountryCard.styles.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Container = styled.div`
+  background-color: ${({ theme }) => theme.colors.elements};
+  color: ${({ theme }) => theme.colors.text};
+  box-shadow: ${({ theme }) => theme.shadow};
+  border-radius: ${({ theme }) => theme.br.cards};
+  min-height: 22rem;
+`;
+
+export const Flag = styled.img`
+  width: 100%;
+  height: 10rem;
+  border-top-left-radius: ${({ theme }) => theme.br.cards};
+  border-top-right-radius: ${({ theme }) => theme.br.cards};
+`;
+
+export const Body = styled.div`
+  padding: 1.5rem;
+`;
+
+export const Title = styled(Link)`
+  font-weight: ${({ theme }) => theme.weights.lg};
+  margin-bottom: 1rem;
+  font-size: 1.2rem;
+  text-decoration: none;
+  color: inherit;
+  display: block;
+`;
+
+export const Data = styled.span`
+  display: block;
+  font-size: 0.875rem;
+
+  &:not(:last-child) {
+    margin-bottom: 0.25rem;
+  }
+`;
+
+export const DataName = styled.span`
+  font-weight: ${({ theme }) => theme.weights.md};
+`;
