9f2d08555b5fbb4643af4cf4a21e94d71e97c64f
Alex Bojorquez <Alex.bojorquez2598@gmail.com>
2023-03-16 16:24:56 -0400

    setup app, global font, and updated packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import Theme from "./contexts/Theme";
+import { CountriesProvider } from "./contexts/Countries";
+import Header from "./components/Header/Header";
+import CountryDirectory from "./components/CountryDirectory/CountryDirectory";
+import CountryPage from "./components/CountryPage/CountryPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Theme>
+        <CountriesProvider>
+          <Header />
+          <Routes>
+            <Route index element={<CountryDirectory />} />
+            <Route path="/country/:countryName" element={<CountryPage />} />
+          </Routes>
+        </CountriesProvider>
+      </Theme>
+    </BrowserRouter>
+  );
 }
 
 export default App;
